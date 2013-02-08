96c5879c1777d2699366a9de6a1435e35da4211d
Christopher Patton <chpatton013@gmail.com>
2013-02-08 02:05:56 -0800

    add fucntion prototypes

--- src/RayTracer.h
+++ src/RayTracer.h
@@ -11,6 +11,8 @@
 #include "Type.h"
 #include "Util.h"
 
+#define CUDA_CALLABLE __host__ __device__
+
 void draw_scene(
    light_t* lights, uint16_t light_count,
    sphere_t* spheres, uint16_t sphere_count,
@@ -18,147 +20,34 @@ void draw_scene(
    uint16_t img_w, uint16_t img_h
 );
 
-__host__ __device__ void get_primary_ray_direction(
-   camera_t* camera,
-   uint16_t img_w, uint16_t img_h,
-   uint16_t x, uint16_t y,
-   float* direction
-) {
-   float scaled_camera_right[3];
-   cross(camera->front, camera->up, scaled_camera_right);
-   scale_i(
-      scaled_camera_right,
-      tan(camera->fov[0] * 0.5f) *
-       (((x + 0.5f) * 2.0f / (float)img_w) - 1.0f)
-   );
-
-   float scaled_camera_up[3];
-   scale(
-      camera->up,
-      tan(camera->fov[1] * 0.5f) *
-       (1.0f - ((img_h - y + 0.5f) / (float)img_h) * 2.0f),
-      scaled_camera_up
-   );
-
-   // ray_direction = normalized(
-   //    camera forward + scaled_camera_right + scaled_camera_up
-   // )
-   add(camera->front, scaled_camera_right, direction);
-   add_i(direction, scaled_camera_up);
-   norm_i(direction);
-}
-
-__host__ __device__ float get_closest_intersection(
-   ray_t* ray, sphere_t* spheres, uint16_t sphere_count, sphere_t** target
-) {
-   float closest_param = FLT_MAX;
-   sphere_t* closest_sphere = NULL;
-
-   for (int ndx = 0; ndx < sphere_count; ++ndx) {
-      float current_param = get_ray_sphere_intersection(ray, spheres + ndx);
-
-      if (current_param >= 0.0f && current_param < closest_param) {
-         closest_param = current_param;
-         closest_sphere = spheres + ndx;
-      }
-   }
-
-   if (closest_sphere == NULL) {
-      return -1.0f;
-   } else {
-      *target = closest_sphere;
-      return closest_param;
-   }
-}
-
-__host__ __device__ void light_surface(
-   ray_t* ray, float parameter, sphere_t* sphere,
-   camera_t* camera, light_t* lights, uint16_t light_count,
-   float* color
-) {
-   float position[3];
-   float surface_normal[3];
-   float viewer_direction[3];
-
-   // position = ray position(parameter)
-   get_ray_position(ray, parameter, position);
-   // normal = sphere normal(position)
-   get_sphere_normal(sphere, position, surface_normal);
-   // viewer = normalized(position - camera position)
-   sub(position, camera->position, viewer_direction);
-   norm_i(viewer_direction);
-
-   for (int ndx = 0; ndx < light_count; ++ndx) {
-      float light_direction[3];
-      float reflected_direction[3];
-
-      // light_direction = normalized(position - light position)
-      sub(position, lights[ndx].position, light_direction);
-      norm_i(light_direction);
-
-      // reflected = normalized(
-      //    (2 * (light_direction dot normal) * normal) - light_direction
-      // )
-      scale(
-         surface_normal,
-         (2 * dot(light_direction, surface_normal)),
-         reflected_direction
-      );
-      sub_i(reflected_direction, light_direction);
-      norm_i(reflected_direction);
-
-      float lighting[3];
-      get_phong_lighting(
-         light_direction, surface_normal,
-         reflected_direction, viewer_direction,
-         &sphere->composition, lighting
-      );
-      add_i(color, lighting);
-   }
-}
-
-__host__ __device__ void cast_primary_ray(
-   light_t* lights, uint16_t light_count,
-   sphere_t* spheres, uint16_t sphere_count,
-   camera_t* camera, float* img_buffer,
-   uint16_t img_w, uint16_t img_h,
-   uint16_t x, uint16_t y
-) {
-   ray_t ray;
-   copy(camera->position, ray.origin);
-   get_primary_ray_direction(camera, img_w, img_h, x, y, ray.direction);
-
-   sphere_t* sphere;
-   float intersection = get_closest_intersection(
-      &ray, spheres, sphere_count, &sphere
-   );
-
-   float color[3] = {0.0f, 0.0f, 0.0f};
-   if (intersection >= 0.0f) {
-      light_surface(
-         &ray, intersection, sphere,
-         camera, lights, light_count, color
-      );
-   }
-   copy(color, img_buffer + 3 * (y * img_w + x));
-}
-
 __global__ void draw_scene_kernel(
    sphere_t* d_spheres, uint16_t sphere_count,
    light_t* d_lights, uint16_t light_count,
    camera_t* d_camera, float* d_img_buffer,
    uint16_t img_w, uint16_t img_h
-) {
-   int x = threadIdx.x + blockIdx.x * blockDim.x;
-   int y = threadIdx.y + blockIdx.y * blockDim.y;
+);
 
-   if (x < img_w && y < img_h) {
-      cast_primary_ray(
-         d_lights, light_count, d_spheres, sphere_count,
-         d_camera, d_img_buffer, img_w, img_h, x, y
-      );
-   }
-}
+CUDA_CALLABLE void cast_primary_ray(
+   light_t* lights, uint16_t light_count,
+   sphere_t* spheres, uint16_t sphere_count,
+   camera_t* camera, float* img_buffer,
+   uint16_t img_w, uint16_t img_h,
+   uint16_t x, uint16_t y
+);
+CUDA_CALLABLE void get_primary_ray_direction(
+   camera_t* camera,
+   uint16_t img_w, uint16_t img_h,
+   uint16_t x, uint16_t y,
+   float* direction
+);
+CUDA_CALLABLE float get_closest_intersection(
+   ray_t* ray, sphere_t* spheres, uint16_t sphere_count, sphere_t** target
+);
+CUDA_CALLABLE void light_surface(
+   ray_t* ray, float parameter, sphere_t* sphere,
+   camera_t* camera, light_t* lights, uint16_t light_count,
+   float* color
+);
 
 void draw_scene(
    light_t* lights, uint16_t light_count,
@@ -219,5 +108,149 @@ void draw_scene(
    cudaFree(d_lights);
    cudaFree(d_img_buffer);
 }
+
+__global__ void draw_scene_kernel(
+   sphere_t* d_spheres, uint16_t sphere_count,
+   light_t* d_lights, uint16_t light_count,
+   camera_t* d_camera, float* d_img_buffer,
+   uint16_t img_w, uint16_t img_h
+) {
+   int x = threadIdx.x + blockIdx.x * blockDim.x;
+   int y = threadIdx.y + blockIdx.y * blockDim.y;
+
+   if (x < img_w && y < img_h) {
+      cast_primary_ray(
+         d_lights, light_count, d_spheres, sphere_count,
+         d_camera, d_img_buffer, img_w, img_h, x, y
+      );
+   }
+}
+
+CUDA_CALLABLE void cast_primary_ray(
+   light_t* lights, uint16_t light_count,
+   sphere_t* spheres, uint16_t sphere_count,
+   camera_t* camera, float* img_buffer,
+   uint16_t img_w, uint16_t img_h,
+   uint16_t x, uint16_t y
+) {
+   ray_t ray;
+   copy(camera->position, ray.origin);
+   get_primary_ray_direction(camera, img_w, img_h, x, y, ray.direction);
+
+   sphere_t* sphere;
+   float intersection = get_closest_intersection(
+      &ray, spheres, sphere_count, &sphere
+   );
+
+   float color[3] = {0.0f, 0.0f, 0.0f};
+   if (intersection >= 0.0f) {
+      light_surface(
+         &ray, intersection, sphere,
+         camera, lights, light_count, color
+      );
+   }
+   copy(color, img_buffer + 3 * (y * img_w + x));
+}
+
+CUDA_CALLABLE void get_primary_ray_direction(
+   camera_t* camera,
+   uint16_t img_w, uint16_t img_h,
+   uint16_t x, uint16_t y,
+   float* direction
+) {
+   float scaled_camera_right[3];
+   cross(camera->front, camera->up, scaled_camera_right);
+   scale_i(
+      scaled_camera_right,
+      tan(camera->fov[0] * 0.5f) *
+       (((x + 0.5f) * 2.0f / (float)img_w) - 1.0f)
+   );
+
+   float scaled_camera_up[3];
+   scale(
+      camera->up,
+      tan(camera->fov[1] * 0.5f) *
+       (1.0f - ((img_h - y + 0.5f) / (float)img_h) * 2.0f),
+      scaled_camera_up
+   );
+
+   // ray_direction = normalized(
+   //    camera forward + scaled_camera_right + scaled_camera_up
+   // )
+   add(camera->front, scaled_camera_right, direction);
+   add_i(direction, scaled_camera_up);
+   norm_i(direction);
+}
+
+CUDA_CALLABLE float get_closest_intersection(
+   ray_t* ray, sphere_t* spheres, uint16_t sphere_count, sphere_t** target
+) {
+   float closest_param = FLT_MAX;
+   sphere_t* closest_sphere = NULL;
+
+   for (int ndx = 0; ndx < sphere_count; ++ndx) {
+      float current_param = get_ray_sphere_intersection(ray, spheres + ndx);
+
+      if (current_param >= 0.0f && current_param < closest_param) {
+         closest_param = current_param;
+         closest_sphere = spheres + ndx;
+      }
+   }
+
+   if (closest_sphere == NULL) {
+      return -1.0f;
+   } else {
+      *target = closest_sphere;
+      return closest_param;
+   }
+}
+
+CUDA_CALLABLE void light_surface(
+   ray_t* ray, float parameter, sphere_t* sphere,
+   camera_t* camera, light_t* lights, uint16_t light_count,
+   float* color
+) {
+   float position[3];
+   float surface_normal[3];
+   float viewer_direction[3];
+
+   // position = ray position(parameter)
+   get_ray_position(ray, parameter, position);
+   // normal = sphere normal(position)
+   get_sphere_normal(sphere, position, surface_normal);
+   // viewer = normalized(position - camera position)
+   sub(position, camera->position, viewer_direction);
+   norm_i(viewer_direction);
+
+   for (int ndx = 0; ndx < light_count; ++ndx) {
+      float light_direction[3];
+      float reflected_direction[3];
+
+      // light_direction = normalized(position - light position)
+      sub(position, lights[ndx].position, light_direction);
+      norm_i(light_direction);
+
+      // reflected = normalized(
+      //    (2 * (light_direction dot normal) * normal) - light_direction
+      // )
+      scale(
+         surface_normal,
+         (2 * dot(light_direction, surface_normal)),
+         reflected_direction
+      );
+      sub_i(reflected_direction, light_direction);
+      norm_i(reflected_direction);
+
+      float lighting[3];
+      get_phong_lighting(
+         light_direction, surface_normal,
+         reflected_direction, viewer_direction,
+         &sphere->composition, lighting
+      );
+      add_i(color, lighting);
+   }
+}
+
+#undef CUDA_CALLABLE
 
 #endif
--- src/Util.h
+++ src/Util.h
@@ -5,6 +5,61 @@
 #include <stdint.h>
 #include <cuda.h>
 
+#define CUDA_CALLABLE __host__ __device__
+
+/**
+ * A fast approximation for the inverse square root.
+ */
+CUDA_CALLABLE float Q_rsqrt(float number);
+
+/**
+ * Vector math
+ */
+CUDA_CALLABLE void zero_vector(float* v);
+CUDA_CALLABLE void copy(float* v_src, float* v_dest);
+CUDA_CALLABLE float dot(float* v1, float* v2);
+CUDA_CALLABLE void cross(float* v1, float* v2, float* v_dest);
+CUDA_CALLABLE float mag(float* v);
+CUDA_CALLABLE void scale(float* v_src, float s, float* v_dest);
+CUDA_CALLABLE void scale_i(float* v, float s);
+CUDA_CALLABLE void scalev(float* v1, float* v2, float* v_dest);
+CUDA_CALLABLE void scalev_i(float* v1, float* v2);
+CUDA_CALLABLE void norm(float* v_src, float* v_dest);
+CUDA_CALLABLE void norm_i(float* v);
+CUDA_CALLABLE void add(float* v1, float* v2, float* v_dest);
+CUDA_CALLABLE void add_i(float* v1, float* v2);
+CUDA_CALLABLE void sub(float* v1, float* v2, float* v_dest);
+CUDA_CALLABLE void sub_i(float* v1, float* v2);
+
+/**
+ * Type-specific
+ */
+CUDA_CALLABLE void get_phong_lighting(
+   float* light_direction, float* surface_normal,
+   float* reflected_direction, float* viewer_direction,
+   composition_t* composition, float* color
+);
+CUDA_CALLABLE void get_ambient_lighting(
+   composition_t* composition, float* color
+);
+CUDA_CALLABLE void get_diffuse_lighting(
+   float* light_direction, float* surface_normal,
+   composition_t* composition, float* color
+);
+CUDA_CALLABLE void get_specular_lighting(
+   float* reflected_direction, float* viewer_direction,
+   composition_t* composition, float* color
+);
+CUDA_CALLABLE void get_ray_position(
+   ray_t* ray, float parameter, float* position
+);
+CUDA_CALLABLE void get_sphere_normal(
+   sphere_t* sphere, float* position, float* normal
+);
+CUDA_CALLABLE float get_ray_sphere_intersection(
+   ray_t* ray, sphere_t* sphere
+);
+
 /**
  * This disgusting function returns a very good approximation of the inverse
  * square root.
@@ -17,7 +72,7 @@
  * use a magic number in some math and bitshifting, reinterpret the value
  * as a float, and apply a couple iterations of Newton's method for accuracy.
  */
-__host__ __device__ float Q_rsqrt(float number) {
+CUDA_CALLABLE float Q_rsqrt(float number) {
    long i;
    float x2, y;
    float threehalfs = 1.5f;
@@ -36,19 +91,19 @@ __host__ __device__ float Q_rsqrt(float number) {
 /**
  * Vector math
  */
-__host__ __device__ void zero_vector(float* v) {
+CUDA_CALLABLE void zero_vector(float* v) {
    for (int ndx = 0; ndx < 3; ++ndx) {
       v[ndx] = 0.0f;
    }
 }
 
-__host__ __device__ void copy(float* v_src, float* v_dest) {
+CUDA_CALLABLE void copy(float* v_src, float* v_dest) {
    for (int ndx = 0; ndx < 3; ++ndx) {
       v_dest[ndx] = v_src[ndx];
    }
 }
 
-__host__ __device__ float dot(float* v1, float* v2) {
+CUDA_CALLABLE float dot(float* v1, float* v2) {
    float result = 0.0f;
 
    for (int ndx = 0; ndx < 3; ++ndx) {
@@ -58,68 +113,68 @@ __host__ __device__ float dot(float* v1, float* v2) {
    return result;
 }
 
-__host__ __device__ void cross(float* v1, float* v2, float* v_dest) {
+CUDA_CALLABLE void cross(float* v1, float* v2, float* v_dest) {
    // Sarrus' Rule for 3x3 determinants
    v_dest[0] = v1[1] * v2[2] - v1[2] * v2[1];
    v_dest[1] = v1[2] * v2[0] - v1[0] * v2[2];
    v_dest[2] = v1[0] * v2[1] - v1[1] * v2[0];
 }
 
-__host__ __device__ float mag(float* v) {
+CUDA_CALLABLE float mag(float* v) {
    return 1 / Q_rsqrt(dot(v, v));
 }
 
-__host__ __device__ void scale(float* v_src, float s, float* v_dest) {
+CUDA_CALLABLE void scale(float* v_src, float s, float* v_dest) {
    for (int ndx = 0; ndx < 3; ++ndx) {
       v_dest[ndx] = v_src[ndx] * s;
    }
 }
 
-__host__ __device__ void scale_i(float* v, float s) {
+CUDA_CALLABLE void scale_i(float* v, float s) {
    for (int ndx = 0; ndx < 3; ++ndx) {
       v[ndx] *= s;
    }
 }
 
-__host__ __device__ void scalev(float* v1, float* v2, float* v_dest) {
+CUDA_CALLABLE void scalev(float* v1, float* v2, float* v_dest) {
    for (int ndx = 0; ndx < 3; ++ndx) {
       v_dest[ndx] = v1[ndx] * v2[ndx];
    }
 }
 
-__host__ __device__ void scalev_i(float* v1, float* v2) {
+CUDA_CALLABLE void scalev_i(float* v1, float* v2) {
    for (int ndx = 0; ndx < 3; ++ndx) {
       v1[ndx] *= v2[ndx];
    }
 }
 
-__host__ __device__ void norm(float* v_src, float* v_dest) {
+CUDA_CALLABLE void norm(float* v_src, float* v_dest) {
    scale(v_src, 1.0f / mag(v_src), v_dest);
 }
 
-__host__ __device__ void norm_i(float* v) {
+CUDA_CALLABLE void norm_i(float* v) {
    scale_i(v, 1.0f / mag(v));
 }
 
-__host__ __device__ void add(float* v1, float* v2, float* v_dest) {
+CUDA_CALLABLE void add(float* v1, float* v2, float* v_dest) {
    for (int ndx = 0; ndx < 3; ++ndx) {
       v_dest[ndx] = v1[ndx] + v2[ndx];
    }
 }
 
-__host__ __device__ void add_i(float* v1, float* v2) {
+CUDA_CALLABLE void add_i(float* v1, float* v2) {
    for (int ndx = 0; ndx < 3; ++ndx) {
       v1[ndx] += v2[ndx];
    }
 }
 
-__host__ __device__ void sub(float* v1, float* v2, float* v_dest) {
+CUDA_CALLABLE void sub(float* v1, float* v2, float* v_dest) {
    for (int ndx = 0; ndx < 3; ++ndx) {
       v_dest[ndx] = v1[ndx] - v2[ndx];
    }
 }
 
-__host__ __device__ void sub_i(float* v1, float* v2) {
+CUDA_CALLABLE void sub_i(float* v1, float* v2) {
    for (int ndx = 0; ndx < 3; ++ndx) {
       v1[ndx] -= v2[ndx];
    }
@@ -128,39 +183,7 @@ __host__ __device__ void sub_i(float* v1, float* v2) {
 /**
  * Type-specific
  */
-__host__ __device__ void get_ambient_lighting(
-   composition_t* composition, float* color
-) {
-   copy(composition->ambient, color);
-}
-
-__host__ __device__ void get_diffuse_lighting(
-   float* light_direction, float* surface_normal,
-   composition_t* composition, float* color
-) {
-   float L_dot_N = dot(light_direction, surface_normal);
-
-   if (L_dot_N > 0.0f) {
-      scale(composition->diffuse, L_dot_N, color);
-   } else {
-      zero_vector(color);
-   }
-}
-
-__host__ __device__ void get_specular_lighting(
-   float* reflected_direction, float* viewer_direction,
-   composition_t* composition, float* color
-) {
-   float R_dot_V = dot(reflected_direction, viewer_direction);
-
-   if (R_dot_V > 0.0f) {
-      scale(composition->specular, pow(R_dot_V, composition->shine), color);
-   } else {
-      zero_vector(color);
-   }
-}
-
-__host__ __device__ void get_phong_lighting(
+CUDA_CALLABLE void get_phong_lighting(
    float* light_direction, float* surface_normal,
    float* reflected_direction, float* viewer_direction,
    composition_t* composition, float* color
@@ -183,21 +206,53 @@ __host__ __device__ void get_phong_lighting(
    add_i(color, specular);
 }
 
-__host__ __device__ void get_ray_position(
+CUDA_CALLABLE void get_ambient_lighting(
+   composition_t* composition, float* color
+) {
+   copy(composition->ambient, color);
+}
+
+CUDA_CALLABLE void get_diffuse_lighting(
+   float* light_direction, float* surface_normal,
+   composition_t* composition, float* color
+) {
+   float L_dot_N = dot(light_direction, surface_normal);
+
+   if (L_dot_N > 0.0f) {
+      scale(composition->diffuse, L_dot_N, color);
+   } else {
+      zero_vector(color);
+   }
+}
+
+CUDA_CALLABLE void get_specular_lighting(
+   float* reflected_direction, float* viewer_direction,
+   composition_t* composition, float* color
+) {
+   float R_dot_V = dot(reflected_direction, viewer_direction);
+
+   if (R_dot_V > 0.0f) {
+      scale(composition->specular, pow(R_dot_V, composition->shine), color);
+   } else {
+      zero_vector(color);
+   }
+}
+
+CUDA_CALLABLE void get_ray_position(
    ray_t* ray, float parameter, float* position
 ) {
    scale(ray->direction, parameter, position);
    add_i(position, ray->origin);
 }
 
-__host__ __device__ void get_sphere_normal(
+CUDA_CALLABLE void get_sphere_normal(
    sphere_t* sphere, float* position, float* normal
 ) {
    sub(sphere->center, position, normal);
    norm_i(normal);
 }
 
-__host__ __device__ float get_ray_sphere_intersection(
+CUDA_CALLABLE float get_ray_sphere_intersection(
    ray_t* ray, sphere_t* sphere
 ) {
    float* c = sphere->center;
@@ -228,5 +283,7 @@ __host__ __device__ float get_ray_sphere_intersection(
       return -B / (2 * A);
    }
 }
+
+#undef CUDA_CALLABLE
 
 #endif
